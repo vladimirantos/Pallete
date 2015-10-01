1ba855cff3e9cef487799935a993c98289e89600
Vladimír Antoš <vladimirantos@seznam.cz>
2015-10-01 16:42:17 +0200

    Verze na webu

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -16,6 +16,7 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
+		$router[] = new Route('admin', 'Sign:in');
 		$router[] = new Route('[<locale=cs cs|de|pl|sk>/]<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
